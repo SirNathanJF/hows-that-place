2e94cebdbe63a4854007d0bbdbed1a3c381f38f4
Nathan Flessner <nathanflessner1@gmail.com>
2021-11-23 20:27:39 -0700

    added some typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,4 +2,12 @@ const { AuthenticationError } = require("apollo-server-errors");
 const { User, Pin } = require("../models");
 const { signToken } = require("../utils/auth");
 
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find({});
+    },
+  }
+}
+
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,26 @@
 const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    password: String
+  }
+  type Pin {
+    username: String,
+    title: String,
+    desc: String,
+    rating: Number,
+    long: Number,
+    lat: Number,
+  }
+  type Query {
+    users: [User]
+    pins(userId: ID!): [Pin]
+    pin(pinId: ID!): Pin
+    me: User
+  }
+`;
+
 module.exports = typeDefs;
