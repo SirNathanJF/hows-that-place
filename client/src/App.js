8b64a4b5c8ab0a4de87c3ae55652b9ae8648838c
Nathan Flessner <nathanflessner1@gmail.com>
2021-11-30 20:37:57 -0700

    added map from react-map-gl, customized marker and map, added .env for reac-map-gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { Room } from "@material-ui/icons"
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 4,
+  });
   return (
     <div className="App">
-     <p>Hello</p>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/malkieriqueen/ckwmz82wn0enf14o7z0hmj9bl"
+      >
+        <Marker
+          latitude={37.78}
+          longitude={-122.41}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <Room style={{fontSize: 7 * viewport.zoom, color:"slateblue"}}/>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
