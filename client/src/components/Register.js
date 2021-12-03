7c020c8aa1817bd2b59d9ac899242e25b1e7b2d9
Nathan Flessner <nathanflessner1@gmail.com>
2021-12-02 21:50:18 -0700

    added more to register modal and stylized

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,20 @@
 import "./register.css"
+import { Room } from "@material-ui/icons"
 
 export default function Register() {
   return (
     <div className="registerContainer">
       <div className="logo">
+        <Room />
+        Fara
       </div>
       <form>
         <input type="text" placeholder="username"/>
         <input type="email" placeholder="email"/>
         <input type="password" placeholder="password"/>
-        <button>Register</button>
+        <button className="registerBtn">Register</button>
+        <span className="success">Successful! You may login now!</span>
+        <span className="failure">Error. Something went wrong!</span>
       </form>
     </div>
   )
