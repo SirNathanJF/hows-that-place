d7e2edf70fece0dff90c5db759b50bbb0bfbe9d9
Nathan Flessner <nathanflessner1@gmail.com>
2021-12-02 19:02:58 -0700

    coded in popup close on x of pin

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { Room, Star } from "@material-ui/icons";
 import "./app.css";
 import axios from "axios";
-import {format} from "timeago.js"
+import { format } from "timeago.js";
 
 function App() {
   const [pins, setPins] = useState([]);
-  const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -29,9 +29,9 @@ function App() {
   }, []);
 
   const handleMarkerClick = (id, lat, long) => {
-    setCurrentPlaceId(id)
+    setCurrentPlaceId(id);
     setViewport({ ...viewport, latitude: lat, longitude: long });
-  } 
+  };
 
   return (
     <div className="App">
@@ -50,35 +50,39 @@ function App() {
               offsetTop={-10}
             >
               <Room
-                style={{ fontSize: 7 * viewport.zoom, color: "slateblue" }}
+                style={{ fontSize: 7 * viewport.zoom, color: "slateblue", cursor:"pointer" }}
                 onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
               />
             </Marker>
             {p._id === currentPlaceId && (
-            <Popup
-          latitude={p.lat}
-          longitude={p.long}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="left" >
-          <div className="card">
-            <label>Place</label>
-            <h4 className="place">{p.title}</h4>
-            <label>Review</label>
-            <p className="desc">{p.desc}</p>
-            <label>Rating</label>
-            <div className="stars">
-            <Star className="star" />
-            <Star className="star" />
-            <Star className="star" />
-            <Star className="star" />
-            <Star className="star" />
-            </div>
-            <label>Information</label>
-            <span className="username">Created by <b>{p.username}</b></span>
-            <span className="date">{format(p.createdAt)}</span>
-          </div>
-        </Popup>
+              <Popup
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={()=>setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
             )}
           </div>
         ))}
